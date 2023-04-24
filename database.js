0e5c3f162d54bab6ae866ba692a53f6b74819c72
aman khan <mohdamankhan18@gmail.com>
2023-04-24 16:56:45 +0530

    first commit

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
-const mongouri="mongodb://localhost:27017"
+const mongouri="mongodb+srv://scheduler:C9IixdpHJzfy3jng@cluster0.dmnsn.mongodb.net/job_tracker?retryWrites=true&w=majority"
 
-// mongodb+srv://scheduler:C9IixdpHJzfy3jng@cluster0.dmnsn.mongodb.net/job_tracker?retryWrites=true&w=majority
+
 const connectToMongo=()=>{
     mongoose.connect(mongouri,()=>{
         console.log("connected to mongo")
